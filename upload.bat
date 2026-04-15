b5aaf5bedeb8da16c009197a1414710fac50e4f3
Indulekha <indulekha0606@gmail.com>
2026-04-16 00:39:04 +0530

    Official Release: ChandaLGx Luxury Sanskrit Prosody Intelligence

--- upload.bat
+++ upload.bat
@@ -8,11 +8,11 @@ git init
 git add .
 git commit -m "Official Release: ChandaLGx Luxury Sanskrit Prosody Intelligence"
 git branch -M main
-git remote add origin https://github.com/InduS06/Chanda.git
+git remote add origin https://github.com/indulekha0606S/ChandaLGx.git
 git push -u origin main
 
 echo.
 echo =======================================
-echo    DONE! Refresh https://github.com/InduS06/Chanda
+echo    DONE! Refresh https://github.com/indulekha0606S/ChandaLGx
 echo =======================================
 pause
